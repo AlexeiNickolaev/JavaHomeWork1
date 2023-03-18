6d55706ab80731afa6693aa012c2ddb9e09e016c
Alexei <datlife2015@yandex.ru>
2023-03-18 17:16:16 +0300

    Homework1

--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -1,22 +1,26 @@
 package task2;
 
+//  Дан массив nums = [3,2,2,3] и число val = 3.
+//Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
+//Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного,
+// а остальные - равны ему.
 public class Main {
     public static void main(String[] args) {
-        int[] arr = { 3, 2, 2, 3 };
+        int[] nums = { 3, 2, 2, 3 };
         int val = 3;
-        int end = arr.length - 1;
+        int end = nums.length - 1;
 
         for (int i = 0; i <= end; i++) {
-            if (arr[i] == val) {
-                int temp = arr[i];
-                arr[i] = arr[end];
-                arr[end] = temp;
+            if (nums[i] == val) {
+                int temp = nums[i];
+                nums[i] = nums[end];
+                nums[end] = temp;
                 end--;
                 i--;
             }
         }
 
-        for (int num : arr) {
+        for (int num : nums) {
             System.out.print(num + " ");
         }
     }
